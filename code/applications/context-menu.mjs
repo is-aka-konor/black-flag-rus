koboldpress/black-flag

[#966] Remove V12-compatible code

code/applications/context-menu.mjs
@@ -1,31 +1,10 @@
 /**
  * Specialized ContextMenu class for displaying at mouse position.
  */
-export default class BlackFlagContextMenu extends (foundry.applications?.ui?.ContextMenu ?? ContextMenu) {
+export default class BlackFlagContextMenu extends foundry.applications.ui.ContextMenu {
 	/** @override */
 	_setPosition(html, target, options) {
-		if (game.release.generation > 12) {
-			html.classList.add("black-flag");
-			return this._setFixedPosition(html, target, options);
-		}
-
-		html = html[0];
-		target = target[0];
-
-		document.body.appendChild(html);
-		const { clientWidth, clientHeight } = document.documentElement;
-		const { width, height } = html.getBoundingClientRect();
-
-		const { clientX, clientY } = window.event;
-		const left = Math.min(clientX, clientWidth - width);
-		this._expandUp = clientY + height > clientHeight;
 		html.classList.add("black-flag");
-		html.classList.toggle("expand-up", this._expandUp);
-		html.classList.toggle("expand-down", !this._expandUp);
-		html.style.visibility = "";
-		html.style.insetInlineStart = `${left}px`;
-		if (this._expandUp) html.style.insetBlockEnd = `${clientHeight - clientY}px`;
-		else html.style.insetBlockStart = `${clientY}px`;
-		target.classList.add("context");
+		return this._setFixedPosition(html, target, options);
 	}
 }

is-aka-konor/black-flag-rus

code/applications/context-menu.mjs
@@ -1,31 +1,10 @@
 /**
  * Specialized ContextMenu class for displaying at mouse position.
  */
-export default class BlackFlagContextMenu extends (foundry.applications?.ux?.ContextMenu ?? ContextMenu) {
+export default class BlackFlagContextMenu extends foundry.applications.ux.ContextMenu {
 	/** @override */
 	_setPosition(html, target, options) {
-		if (game.release.generation > 12) {
-			html.classList.add("black-flag");
-			return this._setFixedPosition(html, target, options);
-		}
-
-		html = html[0];
-		target = target[0];
-
-		document.body.appendChild(html);
-		const { clientWidth, clientHeight } = document.documentElement;
-		const { width, height } = html.getBoundingClientRect();
-
-		const { clientX, clientY } = window.event;
-		const left = Math.min(clientX, clientWidth - width);
-		this._expandUp = clientY + height > clientHeight;
 		html.classList.add("black-flag");
-		html.classList.toggle("expand-up", this._expandUp);
-		html.classList.toggle("expand-down", !this._expandUp);
-		html.style.visibility = "";
-		html.style.insetInlineStart = `${left}px`;
-		if (this._expandUp) html.style.insetBlockEnd = `${clientHeight - clientY}px`;
-		else html.style.insetBlockStart = `${clientY}px`;
-		target.classList.add("context");
+		return this._setFixedPosition(html, target, options);
 	}
 }